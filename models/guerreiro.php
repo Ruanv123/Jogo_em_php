223dad05be7cd3cce902e384c39679b4359d0a23
Ruan Victor <ruanvictormr@gmail.com>
2023-04-19 20:38:55 -0300

    enviando arquivos para o git

---
+++ models/guerreiro.php
@@ -0,0 +1,11 @@
+<?php
+class Guerreiro extends Personagem
+{
+
+  public function __construct($nickname)
+  {
+    parent::__construct("Guerreiro", "personagem-03.png", $nickname, 1, 0, 0);
+  }
+
+}
+?>
